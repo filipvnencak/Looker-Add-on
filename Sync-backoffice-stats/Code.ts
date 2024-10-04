0af2ab66f7dc611a7da3682716933c73d89d3ef5
filipvnencak <vnencak.filip@gmail.com>
2024-10-04 15:27:48 +0200

    Delete and add data in actual month

--- Sync-backoffice-stats/Code.ts
+++ Sync-backoffice-stats/Code.ts
@@ -6,18 +6,13 @@ const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
     'X-API-KEY': 'FsSIxImXMWLT2F0rubei'
   }
 };
+const sheetId = '1414ivVHKEVIiUiwPy3mijltDFElU5HWmnuAM4xmU4Y8'
 
 
-function getBackofficeData() {
-  let month = new Date().getMonth();
+async function getBackofficeData() {
+  let month = new Date().getMonth()+1;
   let year = new Date().getFullYear()
 
-  if(month === 0){
-    month = 12
-  }
-  if(month === 12){
-    year = year-1
-  }
   let url = `https://backoffice.zooza.app/bo-v2/looker/companies?year=${year}&month=${month}`;
 
   let response = UrlFetchApp.fetch(url, options)
@@ -30,13 +25,33 @@ function getBackofficeData() {
     ...item,
     date: item.date ? Utilities.formatDate(new Date(item.date), Session.getScriptTimeZone(), "yyyy-MM") : '1970-01-01'
   }));
-
+  deleteActualMonthData();
   storeDataInSheet(formattedData);
   return formattedData;
 }
+function deleteActualMonthData(): void {
+  let sheet = SpreadsheetApp.openById(sheetId).getActiveSheet();
+  let values = sheet.getDataRange().getValues();
+  const today = new Date();
+  const todayMonth = today.getMonth() + 1;
+  const todayYear = today.getFullYear();
+
+  for (let i = values.length - 1; i >= 0; i--) {
+    let row = values[i];
+    let dateCell = row[0]; // Assuming the date is in the first column
+    if (dateCell) { // Check if the cell contains a date
+      var cellDate = new Date(dateCell); // Convert to a Date object
+
+      if (cellDate.getMonth() + 1 === todayMonth && cellDate.getFullYear() === todayYear) {
+        // If the date matches the current month and year, delete the row
+        sheet.deleteRow(i + 2); // Add 2 because i is zero-based and data starts at row 2
+      }
+    }
+  }
+}
 
 function storeDataInSheet(data) {
-  let sheet = SpreadsheetApp.openById('1414ivVHKEVIiUiwPy3mijltDFElU5HWmnuAM4xmU4Y8').getActiveSheet();
+  let sheet = SpreadsheetApp.openById(sheetId).getActiveSheet();
 
   let response = UrlFetchApp.fetch('https://backoffice.zooza.app/bo-v2/looker/companies/schema', options);
   let sampleData = JSON.parse(response.getContentText());
